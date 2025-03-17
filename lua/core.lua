cffc9e12a43ed61556e58636ef3b0688f913f29c
Attila Szobonya <attila@szobonya.hu>
2025-03-17 20:35:45 +0100

    Configure spaces instead of tabs

--- lua/core.lua
+++ lua/core.lua
@@ -6,6 +6,11 @@ if vim.fn.has("termguicolors") == 1 then
   vim.o.termguicolors = true
 end
 
+vim.opt.tabstop = 4       -- Show tabs as 4 spaces
+vim.opt.shiftwidth = 4    -- Indent/outdent by 4 spaces
+vim.opt.expandtab = true  -- Convert tabs to spaces
+vim.opt.softtabstop = 4   -- When pressing <Tab>, insert 4 spaces
+
 require("packer").startup(function(use)
   -- Let packer manage itself
   use "wbthomason/packer.nvim"
