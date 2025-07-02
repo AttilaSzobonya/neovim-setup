346d33b4bca599b664ca5b0fcb3ac3c0b00a9929
Attila Szobonya <attila.szobonya@siemens.com>
2025-07-02 09:37:54 +0200

    Optimize terminal functionality

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,6 +3,31 @@
 
 local opts = { noremap = true, silent = true }
 
+-- Terminal toggle function that preserves terminal buffer
+local function toggle_terminal()
+  local bufnr = vim.fn.bufnr("term://")
+  if bufnr ~= -1 and vim.fn.bufwinnr(bufnr) ~= -1 then
+    -- Terminal is visible, hide it
+    local win_count = vim.fn.winnr('$')
+    if win_count > 1 then
+      -- There are other windows, safe to hide
+      vim.cmd("hide")
+    else
+      -- This is the only window, switch to another buffer instead
+      vim.cmd("enew")
+    end
+  else
+    -- Terminal is not visible, show it
+    if bufnr ~= -1 then
+      -- Terminal buffer exists but not visible, show existing buffer
+      vim.cmd("sbuffer " .. bufnr)
+    else
+      -- No terminal buffer exists, create new one
+      vim.cmd("terminal")
+    end
+  end
+end
+
 -- File explorer and telescope
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope find_files<CR>", opts)
@@ -12,6 +37,14 @@ vim.api.nvim_set_keymap("n", "é", "$", opts)
 vim.api.nvim_set_keymap("v", "é", "$", opts)
 vim.api.nvim_set_keymap("n", "q", "0", opts)
 vim.api.nvim_set_keymap("v", "q", "0", opts)
+
+-- Terminal keymaps (Hungarian keyboard friendly)
+vim.keymap.set("n", "<F12>", toggle_terminal, opts)
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", opts)
+vim.api.nvim_set_keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", opts)
+vim.api.nvim_set_keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", opts)
+vim.api.nvim_set_keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", opts)
+vim.api.nvim_set_keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", opts)
 
 -- LSP keymaps
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
